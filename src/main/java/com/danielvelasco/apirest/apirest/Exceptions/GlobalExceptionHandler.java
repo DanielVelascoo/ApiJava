c0fb78a6592a2d2aea8e762da73b7e80dec5ce2b
Daniel Velasco <danielcamiloeskratos@gmail.com>
2026-07-04 19:44:12 -0500

    Creación del GlobalExceptionHandler el cual se encarga de manejar todas las excepciones proveniente del Service, sin importa de cual sea funciona

---
+++ src/main/java/com/danielvelasco/apirest/apirest/Exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.danielvelasco.apirest.apirest.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ProductoNoEncontradoException.class)
+    public ResponseEntity<String> manejarProductoNoEncontrado(ProductoNoEncontradoException ex){
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> manejarExcepcionesDelValid(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errores = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errores.put(error.getField(), error.getDefaultMessage());
+        });
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errores);
+    }
+}
