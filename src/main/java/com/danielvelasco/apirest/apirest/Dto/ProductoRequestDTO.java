f4f2ee67bf57f3e5d63e3ff21eb39b4e409fc3dd
Daniel Velasco <danielcamiloeskratos@gmail.com>
2026-07-04 19:51:22 -0500

    Uso de las anotaciones de Validacion, siempre van acá en el dto

--- src/main/java/com/danielvelasco/apirest/apirest/Dto/ProductoRequestDTO.java
+++ src/main/java/com/danielvelasco/apirest/apirest/Dto/ProductoRequestDTO.java
@@ -3,6 +3,7 @@ package com.danielvelasco.apirest.apirest.Dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.*;
 
 
 @Data //La Data contiene Getters, Setters, toString, quals y hashCode.
@@ -11,9 +12,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ProductoRequestDTO {
 
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(max = 100, message = "El nombre no puede tener más de 100 Caracteres")
     private String nombre;
+
+    @NotBlank(message = "La descripción es obligatoria")
     private String descripcion;
+
+    @Positive(message = "El numero debe ser mayor a 0, ceroz")
+    @NotNull(message = "El precio es obligatorio")
     private Double precio;
+
+    @NotNull(message = "El stock es obligatorio")
+    @PositiveOrZero(message = "El stock no puede ser negativo")
     private int stock;
 
 }
